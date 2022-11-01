e8532157477890aff2f896d5cc4e14a1dff9781e
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-01 19:57:27 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,29 +1,41 @@
-
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
+import pandas as pd
+
+
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd #manipulacao de dados
 
-x= np.array([[1,2],[1,2], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,2], [-2,2], [2,7], [-4,1], [0,0]])
-y = np.array([15, 15, 15, 3, 4, 3, 3, 15, 3, 4, 4, 7])
+#######################################################
+#                importando os dados do csv 
+dados=pd.read_csv('/content/drive/MyDrive/inteligencia artificial/trabalho iris/Iris.csv')
 
+dados.head()
+classes = dados['Species']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)#quantos nomes tem
+nomesColunas = nomesColunas[1:tamanho-1] #retira o ultimo
+features = dados[nomesColunas] #monta o features
 
-# splitting X and y into training and testing sets
+#########################################################
+#            quebrar os dados em teste e treino
+
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
 
-#Cria um classificador Gaussiano
-model = GaussianNB()
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.6,
+                                                                               random_state=2)
+#criando a floresta
+floresta = RandomForestClassifier(n_estimators=1000) #constroi a floresta
 
-#Treina o modelo usando os dados de treino 
-model.fit(X_train, y_train)
+#treinar a floresta
 
+floresta.fit(features_treino,classes_treino)
 
-#Resultado de previsão 
-teste = np.array([ [1,2],[0,0],[-2,0],[2,7] ])
-#predicted = model.predict([[1,2],[0,0]]) #fazer a previsão em cima desses 2 numeros
-predicted = model.predict(teste) #fazer a previsão em cima desse array teste
-print(predicted)
-
-# comparing actual response values (y_test) with predicted response values (y_pred)
+#testar quanto a floresta acerta
+predicoes = floresta.predict(features_teste)
+#verificar quanto das predicoes foram acertos
 from sklearn import metrics
-print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, predicted)*100)
+
+print(metrics.classification_report(classes_teste,predicoes))
